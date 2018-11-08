5d3023f58c1984ffacaeda32b06334de0460bd79
Bingran Wang <bingran.wang@cern.ch>
2018-11-08 17:48:19 +0100

    add more HLTs in pre-selection (loosening pre-selection)

--- postproc.py
+++ postproc.py
@@ -97,8 +97,76 @@ HLT_paths = [
    "HLT_Ele40_WPTight_Gsf",
    
    # Single Mu
-   "HLT_IsoMu27"
+   "HLT_IsoMu27",
+
+   # HLTs not in our original list
+   "HLT_Ele115_CaloIdVT_GsfTrkIdT",
+   "HLT_Ele27_WPTight_Gsf",
+   "HLT_Ele32_WPTight_Gsf",
+   "HLT_Ele32_WPTight_Gsf_L1DoubleEG",
+   "HLT_Photon200",
+   "HLT_IsoMu24",
+   "HLT_IsoMu30",
+   "HLT_Mu50",
+   "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass3p8",
+   "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass8",
+   "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ",
+   "HLT_DiEle27_WPTightCaloOnly_L1DoubleEG",
+   "HLT_DoubleEle25_CaloIdL_MW",
+   "HLT_DoublePhoton70",
+   "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL",
+   "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ",
+   "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL",
+   "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL"
 ] 
+
+dic_HLT_NotIn = {
+   "SingleElectron": [
+      "HLT_Ele32_WPTight_Gsf",
+      "HLT_IsoMu30",
+      "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass3p8",
+      "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass8",
+      "HLT_DoubleEle25_CaloIdL_MW",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL"
+   ],
+   "SingleMuon": [
+      "HLT_Ele32_WPTight_Gsf",
+      "HLT_IsoMu30",
+      "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass3p8",
+      "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass8",
+      "HLT_DoubleEle25_CaloIdL_MW",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL"
+   ],
+   "DoubleMuon": [
+      "HLT_Ele32_WPTight_Gsf",
+      "HLT_IsoMu30",
+      "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass3p8",
+      "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass8",
+      "HLT_DoubleEle25_CaloIdL_MW",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL"
+   ],
+   "MuonEG": [
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL"
+   ],
+   "DoubleEG": [
+      "HLT_Ele32_WPTight_Gsf",
+      "HLT_IsoMu30",
+      "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass3p8",
+      "HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass8",
+      "HLT_DoubleEle25_CaloIdL_MW",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ",
+      "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL"
+   ]
+}
+
+for dataset in dic_HLT_NotIn:
+   if options.isMC or options.dataset == 'X' or dataset in options.dataset:
+      HLT_paths = [ HLT for HLT in HLT_paths if HLT not in dic_HLT_NotIn[dataset] ]
+
 pre_selection = "(Sum$(Electron_pt>20&&abs(Electron_eta)<2.5) + Sum$(Muon_pt>20&&abs(Muon_eta)<2.5)>=1)"
 pre_selection = pre_selection + " && (" + "||".join(HLT_paths) + ")"
 
