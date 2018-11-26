452335ef51c5314473f1fb3f373aa9600bda8422
Bingran Wang <bingran.wang@cern.ch>
2018-11-26 10:11:36 +0100

    add min_delta_R>0.4 requirment between goodJets and goodLeptons

--- get_ROOT_files.py
+++ get_ROOT_files.py
@@ -1,27 +1,27 @@
 import yaml
-import sys
+import argparse
 import os
 
-def get_files():
-   # The top directory which contains all the datasets
-   dir_monoZ = '/eos/cms/store/group/phys_exotica/monoZ/MonoZAnalysis_BladeRunner_MonoZ_2017_1111/'
-   if len(sys.argv) == 2:
-      dir_monoZ = sys.argv[1]
 
-   dict_files = {}
-   for path, subdirs, files in os.walk(dir_monoZ): 
-      sub_path_splited = path.split(dir_monoZ)[1].split('/')
-      if len(sub_path_splited) != 4:
-         continue
-      dataset = sub_path_splited[0]
-      if dataset not in dict_files:
-         dict_files[dataset] = {'files':[]}
-      for name in files:
-         if name.split('.')[-1] == 'root':
-            dict_files[dataset]['files'].append( os.path.join(path, name) )
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dir", type=str, default='/eos/cms/store/group/phys_exotica/monoZ/MonoZAnalysis_BladeRunner_MonoZ_2017_1111/', help="top directory containing all the datasets")
+options = parser.parse_args()
 
-   with open('ROOTfiles.yml', 'w') as outfile:
-      yaml.dump(dict_files, outfile, default_flow_style=False)
+dir_monoZ = options.dir
 
-get_files()
+dict_files = {}
+for path, subdirs, files in os.walk(dir_monoZ): 
+   sub_path_splited = path.split(dir_monoZ)[1].split('/')
+   if len(sub_path_splited) != 4:
+      continue
+   dataset = sub_path_splited[0]
+   if dataset not in dict_files:
+      dict_files[dataset] = {'files':[]}
+   for name in files:
+      if name.split('.')[-1] == 'root':
+         dict_files[dataset]['files'].append( os.path.join(path, name) )
+
+with open('ROOTfiles.yml', 'w') as outfile:
+   yaml.dump(dict_files, outfile, default_flow_style=False)
+
 
