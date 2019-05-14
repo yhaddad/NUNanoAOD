b86dcb5d4dd296b18cd81e5a7287efa70ce33d66
Bingran Wang <bingran.wang@cern.ch>
2019-05-14 21:40:29 +0200

    not sure what I'm doing

--- crab/submit_all.py
+++ crab/submit_all.py
@@ -15,13 +15,13 @@ config.JobType.psetName        = 'PSet.py'
 config.JobType.scriptExe       = 'crab_script.sh'
 
 config.JobType.scriptArgs=['isMC={ISMC}','era=2017','doSyst=0','dataset={DATASETARG}','catalog=catalogue_2017.yaml']
-config.JobType.inputFiles=['../keep_and_drop.txt','../keep_and_drop_post.txt','../postproc.py','../haddnano.py']
+config.JobType.inputFiles=['../scripts/keep_and_drop.txt','../scripts/drop_all.txt','../scripts/postproc.py','../scripts/haddnano.py', '../data/combineHLT_2017.yaml']
 config.JobType.sendPythonFolder	       = True
 
 config.Data.inputDataset               = '{DATASET}'
 config.Data.inputDBS                   = 'global'
 config.Data.splitting                  = 'FileBased'
-# config.Data.lumiMask                   = '../configs/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
+# config.Data.lumiMask                   = '../data/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
 config.Data.unitsPerJob                = 1
 config.Data.outLFNDirBase              = '/store/group/phys_exotica/monoZ/MonoZAnalysis_{OUTPUTTAG}/'
 config.Data.publication                = False
@@ -30,7 +30,8 @@ config.Data.allowNonValidInputDataset  = True
 
 config.JobType.allowUndistributedCMSSW = True
 config.Site.storageSite                = 'T2_CH_CERN'
-config.Site.blacklist = ['T2_US_Caltech','T1_DE_KIT','T1_RU_JINR','T2_US_Wisconsin',"T1_DE_KIT"]
+# config.Site.blacklist = ['T2_US_Caltech','T1_DE_KIT','T1_RU_JINR','T2_US_Wisconsin',"T1_DE_KIT"]
+# config.Site.blacklist = ["T1_DE_KIT", "T2_US_Wisconsin", "T2_US_Caltech", "T2_US_Nebraska", "T2_US_Vanderbilt"]
 """
 
 
@@ -49,14 +50,14 @@ with open(options.inputs, 'r') as stream:
 	tag = sample.split("/")[1]
 	if not options.isMC:
 		tag = sample.split("/")[1] + "_" + sample.split("/")[2]
-	crab_config = crab_template.replace("{JOBNAME}", "BladeRunner_1111" + tag)
+	crab_config = crab_template.replace("{JOBNAME}", "Paprika_" + tag)
 	if options.isMC:
 		crab_config = crab_config.replace("{DATASETARG}", 'X')
 	else:
 		crab_config = crab_config.replace("{DATASETARG}", sample)
         crab_config = crab_config.replace("{DATASET}", sample)
         crab_config = crab_config.replace("{ISMC}", str(options.isMC) )
-        crab_config = crab_config.replace("{OUTPUTTAG}", "BladeRunner_MonoZ_2017_1111")
+        crab_config = crab_config.replace("{OUTPUTTAG}", "Paprika_MonoZ_2017")
         if not options.isMC:
 	 	crab_config = crab_config.replace("# config.Data.lumiMask", "config.Data.lumiMask")
 	crab_file = "job_submit_%s.py" % tag
